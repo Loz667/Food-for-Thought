5b6da96ce756b58ab2fa30e40c136cc9744b6664
Loren Penny <loren_667@hotmail.com>
2023-10-30 14:19:02 +0000

    Project update
    Rebooting this project as previous work wasn't aligned with original intention

--- Food for Thought/Assets/Scripts/KitchenObjectSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class KitchenObjectSO : ScriptableObject
-{
-    public string objectName;
-    public Transform objectPrefab;
-    public Sprite objectSprite;
-}
